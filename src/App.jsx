f677b8343e4d549d6259f146e89ba49521db3621
milon27 <mdjahidulislammilon@gmail.com>
2023-05-30 13:54:02 +0600

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,25 @@ const App = () => {
   return (
     <>
       <section className='container bg-slate-500 mx-auto h-screen grid grid-cols-1 grid-rows-6'>
-        {/* menu  */}
-        <nav className='row  bg-blue-400'>
+        {/* nav part */}
+        <nav className='row  bg-blue-400 flex items-center justify-between '>
           {/* logo */}
           <img className='w-20' src="/img/logo.png" alt="" />
           {/* menu  */}
-          <ul className=''>
+          <ul className='flex gap-3 items-center'>
+            <li className=''><a href="">Home</a></li>
+            <li className=''><a href="">Home</a></li>
             <li className=''><a href="">Home</a></li>
           </ul>
         </nav>
         {/* content  */}
         <div className=' bg-red-400'>
-          <div>
+          <div className='text-center'>
             <p>Lorem, ipsum dolor.</p>
             <h1>Lorem ipsum dolor sit.</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
           </div>
-          <div>
+          <div className='flex justify-center items-center'>
             <button>contact us</button>
             <button>contact us</button>
           </div>
