e3a053c06948ffcc465188e5c92ddb5ff3909145
milon27 <mdjahidulislammilon@gmail.com>
2023-06-01 09:22:37 +0600

    all layout with grid done

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
     <>
       <section style={{ backgroundImage: 'url("/img/background.jpg")' }} className='mx-auto min-h-screen grid grid-cols-1 bg-black/50 bg-blend-overlay text-white px-12 py-4'>
         {/* nav part */}
-        <nav className='flex justify-between self-start '>
+        <nav className='grid grid-cols-2 items-center self-start '>
           {/* logo */}
           <img className='h-28' src="/img/logo.png" alt="" />
           {/* menu  */}
-          <ul className='flex gap-3 items-center'>
+          <ul className='grid grid-cols-3 justify-self-end gap-4'>
             <li className=''><a href="">Home</a></li>
             <li className=''><a href="">Home</a></li>
             <li className=''><a href="">Home</a></li>
@@ -21,14 +21,12 @@ const App = () => {
             <p className='text-xl font-medium'>Lorem, ipsum dolor.</p>
             <h1 className='text-5xl'>Lorem ipsum dolor sit.</h1>
             <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
-            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
-            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
-            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
-            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut!</p>
+            
+            
           </div>
-          <div className='flex justify-center items-center my-8'>
-            <button className='px-4 py-2 bg-teal-400 rounded m-2'>contact us</button>
-            <button className='px-4 py-2 bg-teal-400 rounded m-2'>contact us</button>
+          <div className='grid grid-cols-2 justify-center items-center my-8'>
+            <button className='px-4 py-2 w-1/3 justify-self-end bg-teal-400 rounded m-2'>contact us</button>
+            <button className='px-4 py-2 w-1/3 bg-teal-400 rounded m-2'>contact us</button>
           </div>
         </div>
       </section>
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div>
       <App />
       
-      <p className='container mx-auto'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem recusandae eligendi minus dolorum odit veniam odio, deserunt, aliquid sed repudiandae nostrum aliquam nemo. Aut nisi id sequi deleniti doloribus in!
+      <p className='mx-12 my-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem recusandae eligendi minus dolorum odit veniam odio, deserunt, aliquid sed repudiandae nostrum aliquam nemo. Aut nisi id sequi deleniti doloribus in!
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sit fugit quas debitis unde saepe maiores eius atque excepturi dolores eos, aspernatur, dolor explicabo molestias doloribus perspiciatis quis aliquam repudiandae.
       </p>
     </div>
